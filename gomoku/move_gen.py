56ee515ade1febef31db9bd58adb00e9539ee463
YaruG1022 <yaru.gao@wsu.edu>
2026-03-26 09:13:50 -0700

    Implement get_candidate_moves

--- gomoku/move_gen.py
+++ gomoku/move_gen.py
@@ -21,7 +21,31 @@ def get_candidate_moves(
     radius:
         Manhattan neighbourhood radius around existing stones to include.
     """
-    # TODO: iterate over all non-empty cells
-    # TODO: for each stone, add empty neighbours within *radius* to a set
-    # TODO: return sorted / deduplicated candidate list
-    raise NotImplementedError
+    occupied_cells = [
+        (row, col)
+        for row in range(board.size)
+        for col in range(board.size)
+        if board.grid[row][col] != EMPTY
+    ]
+
+    if not occupied_cells:
+        center = board.size // 2
+        return [(center, center)]
+
+    candidates: set[tuple[int, int]] = set()
+    for row, col in occupied_cells:
+        for row_offset in range(-radius, radius + 1):
+            for col_offset in range(-radius, radius + 1):
+                next_row = row + row_offset
+                next_col = col + col_offset
+                if board.is_valid_move(next_row, next_col):
+                    candidates.add((next_row, next_col))
+
+    if not candidates:
+        return board.get_empty_cells()
+
+    center = board.size // 2
+    return sorted(
+        candidates,
+        key=lambda move: (abs(move[0] - center) + abs(move[1] - center), move[0], move[1]),
+    )
